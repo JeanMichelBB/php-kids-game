778908e31d23d14fc97f767f98da08c336227a17
Jean-Michel BB <jeanmichelbberube@outlook.com>
2023-03-22 15:33:56 -0400

    InProgress

--- www/pages/components/footer.php
+++ www/pages/components/footer.php
@@ -1,4 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Include Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
 <?php
 function createFooter() {
-    echo "This is the footer";
+// different color for the footer. Developed by Any Ruiz, Jean-Michel Bérubé, Hallan
+    echo <<<EOT
+    <div class="mt-5"></div>
+    <footer class="bg-dark text-white text-center text-lg-start">
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            Developed by Any Ruiz, Jean-Michel Bérubé, Hallan
+        </div>
+    
+EOT;
 }
+?>
+</body>
+</html>
--- www/pages/components/header.php
+++ www/pages/components/header.php
@@ -1,4 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Include Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+<body>
 <?php
 function createHeader() {
-    echo "This is the header";
+// header with the logo and the title of the website "Kid's game"
+    echo <<<EOT
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="d-flex flex-row justify-content-left"> 
+                        <div class="p-2">
+                            <img src="../../IMG/LogoKidGame.png" alt="logo" width="100" height="100">
+                        </div>
+
+                        <div class="p-2">
+                            <h1 class="text-center">Kid's Game</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+EOT;
 }
+?>
+</body>
+</html>
