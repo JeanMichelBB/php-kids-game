cfd717d75376a3a57660f45b8bf257a83fb3a6bf
Jean-Michel BB <jeanmichelbberube@outlook.com>
2023-03-25 22:06:30 -0400

    adding header footer, and nav

--- www/pages/components/header.php
+++ www/pages/components/header.php
@@ -11,13 +11,13 @@
 <body>
 <?php
 function createHeader() {
-// header with the logo and the title of the website "Kid's game"
+// header with the logo (img src="../../IMG/LogoKidGame.png" alt="logo" width="100" height="100") end left and the title "Kid's game" middle
     echo <<<EOT
     <header>
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="d-flex flex-row justify-content-left"> 
+                    <div class="d-flex flex-row justify-content-space-evenly align-items-center">
                         <div class="p-2">
                             <img src="../../IMG/LogoKidGame.png" alt="logo" width="100" height="100">
                         </div>
--- www/pages/components/nav.php
+++ www/pages/components/nav.php
@@ -1,4 +1,35 @@
-<?php
-function createNav() {
-    echo "This is the nav";
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> 
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    function createNav() {
+
+        echo <<<EOT
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom: 150px;"> <!-- TODO MARGIN bottom-->
+            <a class="navbar-brand" href="#"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="../pages/login.php">Login </a>
+                    <a class="nav-link" href="../pages/registration.php">Registration </a>
+                    <a class="nav-link href="../pages/history.php">History </a>
+                </div>
+            </div>
+        </nav>
+
+        EOT;
+    }
+    ?>
+</body>
+</html>
