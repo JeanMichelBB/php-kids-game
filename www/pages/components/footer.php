9ed3bc93a753129ed700b72748bdf01e92d01021
anyruizd <anyruizd@gmail.com>
2023-03-29 10:53:25 -0400

    fix footer and overall layout

--- www/pages/components/footer.php
+++ www/pages/components/footer.php
@@ -1,7 +1,7 @@
 <?php
 function createFooter() {
     return 
-    '<div class="mt-5"></div>
+    '<div class="fixed-bottom mt-5"></div>
     <footer class="bg-dark text-white text-center text-lg-start" >
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
             Developed by Any Ruiz, Jean-Michel Bérubé, Allan Ricardo Azevedo Barcelos
