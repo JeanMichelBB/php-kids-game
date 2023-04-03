1181faf4d32ef8d22690c13a08fcac90cb2771d6
Jean-Michel Bérubé <107640908+JeanMichelBB@users.noreply.github.com>
2023-04-02 23:14:52 -0400

    change DB info

--- db/login_info.php
+++ db/login_info.php
@@ -1,10 +1,10 @@
 <?php
-define('DB_HOST','db');
-define('DB_USER','root');
-define('DB_PASSWORD','dw3');
+// define('DB_HOST','db');
+// define('DB_USER','root');
+// define('DB_PASSWORD','dw3');
 
 // Uncomment if you are using XAMPP
 
-// define('DB_HOST','localhost');
-// define('DB_USER','root');
-// define('DB_PASSWORD','');
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','');
